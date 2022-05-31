40a9df7e8ca601b42bdbb1f07ad87a546609fa70
Islombek Tadjiyev <96186961+Tadjiyev1312@users.noreply.github.com>
2022-05-31 16:12:39 +0500

    +++++

--- src/views/AddBestProject.vue
+++ src/views/AddBestProject.vue
@@ -1,3 +1,130 @@
 <template>
-    
+  <v-sheet color="#fafbfe">
+    <v-container class="px-10">
+      <h1 class="dark--text py-10">Eng yaxshi loyihani qo'shish</h1>
+
+      <!-- Jamoa a'zosining ismi -->
+
+      <label>Eng yaxshi loyihaning turi Rus tilida</label>
+      <v-text-field
+        outlined
+        v-model="nameRus"
+        :counter="25"
+        :rules="nameRules"
+        label="Eng yaxshi loyihaning turi Rus tilida"
+        required
+        class="mt-6"
+      ></v-text-field>
+      <label>Eng yaxshi loyihaning turi English tilida</label>
+      <v-text-field
+        outlined
+        v-model="nameingliz"
+        :counter="25"
+        :rules="nameRules"
+        label="Eng yaxshi loyihaning turi English tilida"
+        required
+        class="mt-6"
+      ></v-text-field>
+
+      <hr class="danger--text hr" />
+
+      <!-- Jamoa a'zosining kasbi -->
+      <label>Eng yaxshi loyihaning sarlavhasi Rus tilida</label>
+      <v-text-field
+        outlined
+        v-model="kasbiRus"
+        :counter="25"
+        :rules="nameRules"
+        label="Eng yaxshi loyihaning sarlavhasi Rus tilida"
+        required
+        class="mt-6"
+      ></v-text-field>
+      <label>Eng yaxshi loyihaning sarlavhasi Ingliz tilida</label>
+      <v-text-field
+        outlined
+        v-model="kasbiIngliz"
+        :counter="25"
+        :rules="nameRules"
+        label="Eng yaxshi loyihaning sarlavhasi Ingliz tilida"
+        required
+        class="mt-6"
+      ></v-text-field>
+      <hr class="danger--text hr" />
+      <label>Eng yaxshi loyihaning rasmi</label>
+      <v-file-input
+        label="Eng yaxshi loyihaning rasmi"
+        outlined
+        dense
+        class="mt-6"
+      ></v-file-input>
+      <hr class="danger--text hr" />
+      <label form="#">Jamoa a'zosining E-maili</label>
+      <v-text-field
+        outlined
+        v-model="email"
+        :rules="emailRules"
+        label="jamoa a'zosining E-maili"
+        required
+        class="mt-6"
+      ></v-text-field>
+      <hr class="danger--text hr" />
+      <label for="#">Eng yaxshi loyiha haqida Rus tilida</label>
+      <v-textarea
+        outlined
+        name="input-7-4"
+        label="Eng yaxshi loyiha haqida Rus tilida"
+        class="my-10"
+      ></v-textarea>
+      <label>Eng yaxshi loyiha haqida Ingliz tilida</label>
+      <v-textarea
+        outlined
+        name="input-7-4"
+        label="Eng yaxshi loyiha haqida Ingliz tilida"
+        class="my-10"
+      ></v-textarea>
+      <v-btn depressed color="primary"> Yuklash </v-btn>
+    </v-container>
+  </v-sheet>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: true,
+      name: "",
+      nameRus: "",
+      nameIngliz: "",
+      kasbiRus: "",
+      kasbiIngliz: "",
+      nameRules: [
+        (v) => !!v || "Name is required",
+        (v) => (v && v.length <= 15) || "Name must be less than 25 characters",
+      ],
+
+      email: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.hr {
+  height: 3px !important;
+  width: 100% !important;
+  background-color: red !important;
+  border-radius: 5px !important;
+  margin-bottom: 20px;
+}
+h1 {
+  font-family: sans-serif;
+  opacity: 0.6;
+}
+label {
+  opacity: 0.8;
+}
+</style>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,3 +1,80 @@
 <template>
-    
+  <v-container>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead class="py-16 template_bg">
+          <tr>
+            <th class="text-left font-weight-medium h6" colspan="3">
+              <h3>Rasm</h3>
+            </th>
+            <th class="text-left" colspan="3">
+              <h3>Ism</h3>
+            </th>
+            <th class="text-left" colspan="1">
+              <h3>Kasbi</h3>
+            </th>
+            <th class="text-left" colspan="1">
+              <h3>Haqida</h3>
+            </th>
+            <th class="text-left" colspan="1">
+              <h3>Tahrirlash</h3>
+            </th>
+          </tr>
+        </thead>
+        <tbody class="template_bg">
+          <tr v-for="item in desserts" :key="item.ru" class="py-10">
+            <td class="" colspan="3">{{ item.eng }}</td>
+            <td colspan="3">{{ item.ru }}</td>
+            <td colspan="1">
+              <v-icon>{{ item.icondelete }}</v-icon
+              ><v-icon class="ml-10">{{ item.iconedit }}</v-icon>
+            </td>
+            <td colspan="1">
+              <v-icon>{{ item.icondelete }}</v-icon
+              ><v-icon class="ml-10">{{ item.iconedit }}</v-icon>
+            </td>
+            <td colspan="1">
+              <v-icon>{{ item.icondelete }}</v-icon
+              ><v-icon class="ml-10">{{ item.iconedit }}</v-icon>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      desserts: [
+        {
+          eng: "Frozen Yogurt",
+          ru: "Замерзший йогурт",
+          icondelete: "mdi-delete-circle-outline",
+          iconedit: "mdi-pencil-outline",
+        },
+        {
+          eng: "Frozen Yogurt",
+          ru: "Замерзший йогурт",
+          icondelete: "mdi-delete-circle-outline",
+          iconedit: "mdi-pencil-outline",
+        },
+        {
+          eng: "Frozen Yogurt",
+          ru: "Замерзший йогурт",
+          icondelete: "mdi-delete-circle-outline",
+          iconedit: "mdi-pencil-outline",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.template_bg {
+  background-color: #fafbfe !important;
+}
+</style>
